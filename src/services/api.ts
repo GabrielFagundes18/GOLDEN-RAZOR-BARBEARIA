5f6074531aab6fd8ea7c0fe2c2e9b8326b18212b
Gabriel Fagundes <gabrielfagundesvv@gmail.com>
2026-05-12 13:24:52 -0300

    colocado url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL:  'https://golden-razor-barbearia-backend-production.up.railway.app', 
+  baseURL:  'https://golden-razor-barbearia-backend.onrender.com', 
 })
